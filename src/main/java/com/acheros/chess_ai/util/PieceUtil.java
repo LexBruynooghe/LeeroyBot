967c9d5cb2541d7b200c3d0b7d4176d1bb01048a
Acheros <lex.bruynooghe@ugent.be>
2023-06-24 00:45:42 +0200

    pieceutil

---
+++ src/main/java/com/acheros/chess_ai/util/PieceUtil.java
@@ -0,0 +1,9 @@
+package com.acheros.chess_ai.util;
+
+import javafx.scene.image.Image;
+
+public class PieceUtil {
+    public static void getImage() {
+//        return new Image(); // TODO
+    }
+}
